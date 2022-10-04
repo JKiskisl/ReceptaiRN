e28cfd369fb141978c286942f0f2142e61c29efc
JKiskisl <smicscl@gmail.com>
2022-10-04 14:03:58 +0300

    Init

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './pages/home';
+import listRecipeCategory from './pages/listRecipecategories';
+import listRecipeSubCat from './pages/listRecipeSubCat.js';
+
+import Receptai from './Receptai.json';
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Recipes" component={listRecipeCategory} />
+        <Stack.Screen name="SubRecipes" component={listRecipeSubCat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ pages/listRecipeSubCat.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React from 'react';
+import { NavigationContainer, useRoute } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Receptai from '../Receptai.json';
+
+import HomeScreen from './home.js';
+
+
+function ListScreen ({navigation, route}) {
+
+
+      const {kategorija} = route.params;
+
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={styles.container}>
+            {Receptai.kategorija.map((item)=>(
+              <View>
+                <Button title={item.name}/>
+              </View>
+            ))}
+
+          </View>
+          <Text>
+            RecipesSubScreen {route.params.kategorija}
+            
+            
+          </Text>
+          <Button
+            title="Go to Home"
+            onPress={() => navigation.navigate('Home', {HomeScreen})}
+          />
+          <Button
+            title="Go back"
+            onPress={() => navigation.goBack()}
+          />
+      </View>
+      );
+    }
+  
+export default ListScreen;
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
